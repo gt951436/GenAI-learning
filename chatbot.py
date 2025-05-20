ff5ded33d84f0b7e1b18c30249a2cc52394c3e56
gt951436 <tyagigarv95@gmail.com>
2025-05-20 13:45:22 +0530

    added chat history

--- chatbot.py
+++ chatbot.py
@@ -7,10 +7,18 @@ load_dotenv()
 llm = HuggingFaceEndpoint(repo_id="microsoft/Phi-3-mini-4k-instruct",task="text-generation")
 model = ChatHuggingFace(llm=llm)
 
+# chat history
+chat_history = []
+
 while True: # program runs until user exits
     user_input = input('You: ')
+    chat_history.append(user_input)
+    
     if user_input == 'exit':
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
     print("AI: ",result.content)
+    
+print("chat History\n", chat_history)
     
