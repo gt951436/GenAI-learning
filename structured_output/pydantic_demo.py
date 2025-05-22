a43443986bf70fd9d100b956f7e6f9ae6ea5a333
gt951436 <tyagigarv95@gmail.com>
2025-05-22 14:40:32 +0530

    pydantic builtin email validation

--- structured_output/pydantic_demo.py
+++ structured_output/pydantic_demo.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from typing import Optional
 
 class Student(BaseModel):
-    name:str  ='Garv'  #default value in pydantic 
-    age:Optional[int]= None
+    name:str = "Garv"    #default value in pydantic 
+    age:Optional[int] = None
+    email:EmailStr
     
-new_student = {'age':12} 
+new_student = {'age':21,'email':'abc@gmail.com'} 
 std = Student(**new_student)
 
 print(std)
-print(type(std))
