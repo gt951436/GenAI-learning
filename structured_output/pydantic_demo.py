cde0556e2eacac55c7386b5c8fb9bc644c84ca8f
gt951436 <tyagigarv95@gmail.com>
2025-05-22 19:08:44 +0530

    pydantic to dict and json format conversion updated

--- structured_output/pydantic_demo.py
+++ structured_output/pydantic_demo.py
@@ -1,12 +1,20 @@
-from pydantic import BaseModel,EmailStr
+from pydantic import BaseModel,EmailStr,Field
 from typing import Optional
 
 class Student(BaseModel):
     name:str = "Garv"    #default value in pydantic 
     age:Optional[int] = None
     email:EmailStr
+    cgpa: float = Field(gt=0,lt=10,default = 5,description="Decimal value representing cgpa")
     
-new_student = {'age':21,'email':'abc@gmail.com'} 
-std = Student(**new_student)
+new_student = {'age':21,
+               'email':'abc@gmail.com',
+                'cgpa':9.19   # validation error --> bcoz cgpa>10
+            } 
+std = Student(**new_student)  # pydantic object
 
-print(std)
+std_dict = dict(std)  # explicit conversion to dict object from pydantic object
+print(std_dict)
+
+std_json = std.model_dump_json()  # pydantic object to json object
+print(std_json)
